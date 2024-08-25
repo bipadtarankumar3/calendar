08e462a6c6b0c6a8499c532b2b4bcc19db68b4fe
unknown <bipadtarankumar3@gmail.com>
2024-08-25 17:26:58 +0530

    mm

---
+++ app/Models/Festivale.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Festivale extends Model
+{
+    use HasFactory;
+}
