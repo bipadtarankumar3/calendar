5fc63881475f1447a8c982bf5155c67645b43433
trilochankumar9 <trilochankumar9@gmail.com>
2024-08-25 16:15:25 +0530

    commit after delete all unnessaery files by tilok

---
+++ database/migrations/2024_08_25_102912_add_user_type_to_users_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('user_type')->nullable()->after('email'); // Add the user_type column, allowing null values
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('user_type'); // Drop the user_type column if the migration is rolled back
+        });
+    }
+};
